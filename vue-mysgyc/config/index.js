2a708372c3d408aa8d7dfdfc30a9b624c0acbf65
Limx <1129891375@qq.com>
2020-01-22 17:03:21 +0800

    i

--- vue-mysgyc/config/index.js
+++ vue-mysgyc/config/index.js
@@ -19,16 +19,6 @@ module.exports = {
                     // 如果接口本身没有/api需要通过pathRewrite来重写了地址
                     "^/apit": ""
                 }
-            },
-            "/apic": {
-                //匹配项
-                target: "http://c.cztvcloud.com", // 接口的域名
-                // secure: false,// 如果是https接口，需要配置这个参数
-                changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-                pathRewrite: {
-                    // 如果接口本身没有/api需要通过pathRewrite来重写了地址
-                    "^/apic": ""
-                }
             }
         },
 
